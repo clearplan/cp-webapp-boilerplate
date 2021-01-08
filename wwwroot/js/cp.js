47d7a362a8acb54aa4d2a9a901ca1830a87a4e8b
Mark Hughes <markhughes@clearplanconsulting.com>
2021-01-08 16:23:50 -0600

    Updated app with new files. Removed bootstrap

---
+++ wwwroot/js/cp.js
@@ -0,0 +1,17 @@
+﻿; (function () {
+
+    function create(node, html, styles) {
+        node = document.createElement(node);
+        return node;
+    }
+
+    function extend(source, target) {
+        for (var key in target) {
+            if (target.hasOwnProperty(key)) {
+                source[key] = target[key];
+            }
+        }
+        return source;
+    }
+
+})();
