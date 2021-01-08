46d05b704f0255b48af9e8b99a62294ee1806669
Mark Hughes <markhughes@clearplanconsulting.com>
2021-01-08 16:21:20 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cp_webapp_boilerplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
